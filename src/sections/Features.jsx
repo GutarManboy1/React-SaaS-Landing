37360b96669126a9e193cde613f48820ea369316
Glenn Torrens <glenntorrens@gmail.com>
2025-04-25 15:29:21 +0900

    fixed header, finished hero

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -24,24 +24,30 @@ const Features = () => {
                     />
                   </div>
                 </div>
-                <p className="caption mb-5 max-md:mb-6">
-                    {caption}
-                </p>
+                <p className="caption mb-5 max-md:mb-6">{caption}</p>
                 <h2 className="max-w-400 mb-7 h3 text-p4 max-md:mb-6 max:md:h5">
-                    {title}
+                  {title}
                 </h2>
-                <p className="mb-11 body-1 max-md:mb-8 max-md:body-3">
-                {text}
-                </p> 
-                <Button icon={button.icon} >{button.title}</Button>
+                <p className="mb-11 body-1 max-md:mb-8 max-md:body-3">{text}</p>
+                <Button icon={button.icon}>{button.title}</Button>
               </div>
             ))}
             <ul className="relative flex justify-around flex-grow px-[5%] border-2 border-s3 rounded-7xl max-md:hidden">
-            <div className="absolute bg-s3/20 top-[38%] left-0 right-0 w-full h-[1px] z-10 " />
+              <div className="absolute bg-s3/20 top-[38%] left-0 right-0 w-full h-[1px] z-10 " />
               {details.map(({ id, title, icon }) => (
-                  <li key={id} className="relative px-4 pt-16 pb-14">
-                    <div />
-                  </li>
+                <li key={id} className="relative px-4 pt-16 pb-14">
+                  <div className="absolute top-8 bottom-0 left-1/2 bg-s3/20 w-[1px] h-full z-10" />
+                  <div className="flex items-center justify-center mx-auto mb-3 border-2 border-s2 rounded-full hover:border-s4 transition-all duration-500 shadow-500 size-20 ">
+                    <img
+                      src={icon}
+                      alt={title}
+                      className="size-17/20 object-contain z-20"
+                    />
+                  </div>
+                  <h3 className="relative z-2 max-w-36 mx-auto text-center base-small uppercase my-0">
+                    {title}
+                  </h3>
+                </li>
               ))}
             </ul>
           </div>
