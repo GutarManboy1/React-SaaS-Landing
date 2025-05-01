333952dec17f8de960aabfff83d25524f2aa650e
Glenn Torrens <glenntorrens@gmail.com>
2025-05-01 21:18:06 +0900

    added faq and started testimonials

---
+++ src/components/TestimonialItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import clsx from 'clsx'
+
+const TestimonialItem = ({item, containerClassName}) => {
+  return (
+    <div className={clsx("relative px-14 pt-11 pb-14 after:absolute after:bottom-0 after:right-0 after:h-0.5 after:w-screen after:bg-s2 after:content-[''] max-md:px-0 max-md:pt-11 after:max-md: -right-4")} >TestimonialItem</div>
+  )
+}
+
+export default TestimonialItem
